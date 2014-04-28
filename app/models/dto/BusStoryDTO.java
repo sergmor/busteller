b4d7aa60f1765e33cf6a8487749cd88555a801db
Daniel <sdm2162@columbia.edu>
2014-04-28 11:26:41 -0400

    DTO Landmark

--- app/models/dto/BusStoryDTO.java
+++ app/models/dto/BusStoryDTO.java
@@ -16,7 +16,7 @@ public class BusStoryDTO{
 	public String busLong;
 	public List<String> placelat;
 	public List<String> placelng;
-
+	public List<String> lmNames;
 
 	public BusStoryDTO(String ref, String buslat, String busLong) {
 		super();		
@@ -26,6 +26,7 @@ public class BusStoryDTO{
 		this.busLong = busLong;
 		placelat = new ArrayList<String>();
 		placelng = new ArrayList<String>();
+		lmNames = new ArrayList<String>();
 		//this.polyline = "{eixF~|gbM@?JNd@RbBf@\\JzBd@z@PvAZvCj@v@L~ATpC`@|@Lt@JZBbCLX@vCNvBtAbElCl@^fCbBvClBz@j@~@n@|BzAp@b@hAt@zBzAt@d@fAr@zBzAdBhATN|BzAdC`B`GzD|BzAp@b@hAv@|BxAzBzA|BzAzBxAzBxA~AfAf@ZdC~AzBzA|BzAzBxAzBzAr@d@fAr@tBnB|BzAxBxAfC~AY~@gC`IOb@Qb@JjALb@b@n@d@^xBxA|BzAzBxAvFtD~CpBvA~BVd@n@j@`@VvF`CfGjCbAT|@D`@AX?vAB`@Db@JRJ\\TrCjBnH|EPLDBJLtAfBLJp@j@jAr@VN`B`ApBbATHFBrAj@RHrD|AlAj@`DnBTP`F`DZTpFnDb@X~@j@`BnChArAf@b@tCbCbAp@|@`@j@NZDd@@dBKf@Bv@ZDDp@v@jCdF?@VfAx@n@f@\\FBrAl@lAXl@HfCDlAJJHJH\\Tv@v@Xh@LVbAjDLXVl@`@j@\\Z|B|AfC`Bj@`@zA`AZNdATr@Bl@BrBJzCNl@DF?ZD^L^Ph@^lAnAb@d@vAzAxB~BrBxBbB`BNJTL??vBp@jCz@lA^~@XtAd@d@HZLdBoF`@sAbDaKDOr@LpBLZ@bCB`DD`DD`DD`DBdA?bBA^Ax@?hAA`DEbDCdDEhDAbBAR`@RNXDFAHAf@a@RODIHc@AcA[k@IIb@wAzCqJbFyOJ]lEiNN]f@}AP]zBxAhCbBdC`BhBlARL|BzAzBxA|BzAzBzA|@l@~@l@zBxA|BzA|BxAzBzA|BzAzBxA|BzAdC`BbBfA`@XzBxAzBxAzBxAxBxAzBxAzBxAdC~AbC`Bz@j@~@l@zBxAxBxAzBxAzBxAzBxAxBxAZNn@d@r@b@\\LxAdA|@l@JBz@p@lAz@v@f@xBxAtBtAtBtArBrAtBtArBtA~BzAhCbB`An@fAr@~B|A~BzAvBvAtBtAtBvAf@}AzA{E~@uC`A}CdAgDfBvAbBrAh@b@fAz@tBdBvB`B|@v@x@p@hCpBz@p@rC|BVRv@n@hDnCr@j@|D`Dj@b@zBfBlAbA~D~CrDxCv@n@r@j@n@h@vAhAPLvBdBp@h@`Av@pB`BvBdB~ApAFDfBvAxAjATPbBrAD?nAbAXTnB|ArAdARPjBvAxBbBNLrAlAZV`Ax@PLlA`AXT`CnBfAz@`BnAlBzAl@b@LJ~BhBHDpDtCn@l@DBbBpAlAf@^Zh@AtC\\VBf@DRAh@M~@i@x@mAGmC";
 	}
 
@@ -42,6 +43,7 @@ public class BusStoryDTO{
 	public void addLandmarks(Landmark landmark) {
 		placelat.add(landmark.latitude.toString());
 		placelng.add(landmark.longitude.toString());
+		lmNames.add(landmark.name);
 	}
 
 	public void addStories(String stories) {
